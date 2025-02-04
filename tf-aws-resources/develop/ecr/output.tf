be10ef43b2628ee2cd22c708fe720e377ff43cd3
breinerHenrique <breinerhenrique@gmail.com>
2025-02-03 23:26:56 -0300

    Feat: Testing new TF resources

--- tf-aws-resources/develop/ecr/output.tf
+++ tf-aws-resources/develop/ecr/output.tf
@@ -1,10 +1,3 @@
-output "repository_name" {
-  value = {
-    for k, v in module.ecr : k => repository_name
-  }
-  description = "Name of the repository."
-}
-
 output "repository_arn" {
   value = {
     for k, v in module.ecr : k => v.repository_arn
