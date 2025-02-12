3f70bd180f7c3da4968dc80b8d705bb7769c64e3
breinerhenrique-pd <desenvolvedores@pdsolucoes.com.br>
2025-02-11 21:11:13 -0300

    Feat: TF creating Role, LogGroup and SG

--- tf-aws-resources/develop/iam-role/output.tf
+++ tf-aws-resources/develop/iam-role/output.tf
@@ -1,4 +1,4 @@
 output "task_execution_role_arn" {
-    value = resource.aws_iam_role.arn
+    value = resource.aws_iam_role.task_execution_role.arn
     description = "The name of role used to all ECS Task"
 }
